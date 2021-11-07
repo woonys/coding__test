deab6f2a42c30e94ba107b9f33b559aded48b32e
woonys <wodns1324@naver.com>
2021-11-07 22:54:09 +0900

    정글 알고리즘 1주차 34번까지

--- jungle_algorithm/27. N-Queen.py
+++ jungle_algorithm/27. N-Queen.py
@@ -2,28 +2,31 @@ from sys import stdin
 
 N = int(stdin.readline())
 
-pos = [0] * N #각 열에 배치한 퀸의 위치
+#pos = [0] * N #각 열에 배치한 퀸의 위치
 flag_a = [False] * N #각 행에 퀸을 배치했는지 체크
 flag_b = [False] * (2*N-1) #(2* N - 1)  => 오른쪽 대각선 방향으로 퀸을 배치했는지 체크 // 최대
 flag_c = [False] * (2*N-1) # 왼쪽 대각선 방향으로 퀸을 배치했는지 체크
+# def put() -> None:
+#     for i in range(N):
+#         print(f'{pos[i]:2}', end='')
+#     print()
 count = 0
-def put() -> None:
-    for i in range(N):
-        print(f'{pos[i]:2}', end='')
-    print()
-
 def set(i: int) -> None:
+    global count
     for j in range(N):
         if (    not flag_a[j]
             and not flag_b[i+j]
             and not flag_c[i-j+(N-1)]):
-            pos[i] = j
+            #pos[i] = j
             if i == (N-1):
-                put()
+                count += 1
             else:
                 flag_a[j] = flag_b[i+j] = flag_c[i-j+(N-1)] = True
                 set(i+1)
                 flag_a[j] = flag_b[i+j] = flag_c[i-j+(N-1)] = False
 
 
-set(0)
+
+if __name__ == '__main__':
+    set(0)
+    print(count)
