2c16a31b189e927bc41fd6e5c5e7988e6696d5e6
woonys <wodns1324@naver.com>
2022-01-24 15:34:37 +0900

    240_fixed

--- python_algorithm_interview/Ch.18/240. Search a 2D Matrix II.py
+++ python_algorithm_interview/Ch.18/240. Search a 2D Matrix II.py
@@ -1,35 +1,66 @@
 '''
-How to solve: bfs로 접근 -> 시간초과로 실패 / 답안 참고: 일일이 순회하면서 정답 찾는 방식
+How to solve: bfs로 접근 -> 시간초과로 실패 -> visited 추가해서 해결 / 답안 참고: 일일이 순회하면서 정답 찾는 방식
 
 (답안 풀이)
 Runtime: 148 ms, faster than 99.63% of Python3 online submissions for Search a 2D Matrix II.
 Memory Usage: 20.7 MB, less than 12.20% of Python3 online submissions for Search a 2D Matrix II.
 
-시간복잡도: O(log n): 한번 움직일 때마다 한 행 or 열을 통으로 날려버림 => 일일이 검사하지 않는다!
+시간복잡도: O(m+n): 한번 움직일 때마다 한 행 or 열을 통으로 날려버림 => 일일이 검사하지 않는다!
 공간복잡도: O(1): in-place에서 해결
+
+(내 풀이: bfs)
+Runtime: 257 ms, faster than 29.47% of Python3 online submissions for Search a 2D Matrix II.
+Memory Usage: 20.7 MB, less than 38.50% of Python3 online submissions for Search a 2D Matrix II.
+시간복잡도: O(M*N) => 최악의 경우 O(N**2) => 전부 다 탐색해야 함.
+공간복잡도: O(1) => in-place
 '''
 
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        # 책 답안 풀이
-        if not matrix:
-            return False
-        
-        row = 0
-        col= len(matrix[0]) -1
-        while row <=len(matrix)-1 and col >= 0:
-            if target == matrix[row][col]:
-                return True
+#     def binarySearch(self, target, arr):
+#         start, end = 0, len(arr)-1
+#         while start <= end:
+#             mid = (start + end) //2
+#             if target == arr[mid]:
+#                 return True
             
-            elif target < matrix[row][col]:
-                col-=1
-            elif target > matrix[row][col]:
-                row+=1
-        return False
+#             if arr[mid] > target:
+#                 end = mid-1
+#             else:
+#                 start = mid+1
+        
+    
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #solution 0: 이분탐색으로 접근해보기
+        # for i in matrix:
+        #     if i[0] > target:
+        #         return False
+        #     if self.binarySearch(target, i):
+        #         return True
+    
         
         
-        # my solution: bfs로 접근 => 시간초과
+        
+        
+        
+        
+#----------------------------------
+        # Solution 1: 답지 풀이
+#         if not matrix:
+#             return False
+        
+#         row = 0
+#         col= len(matrix[0]) -1
+#         while row <=len(matrix)-1 and col >= 0:
+#             if target == matrix[row][col]:
+#                 return True
+            
+#             elif target < matrix[row][col]:
+#                 col-=1
+#             elif target > matrix[row][col]:
+#                 row+=1
+#         return False
+#----------------------------------
+#         #my solution: bfs
         max_width = len(matrix[0])
         max_height = len(matrix)
         #print(max_width, max_height, "max")
@@ -37,6 +68,7 @@ class Solution:
         queue = collections.deque()
         
         root = [0,0]
+        visited = [[False] * max_width for _ in range(max_height)]
         queue.append(root)
         
         while queue:
@@ -47,16 +79,20 @@ class Solution:
             
             elif now < target:
                 if root[0]+1 < max_height:
-                    queue.append([root[0]+1, root[1]])
+                    if not visited[root[0]+1][root[1]]:
+                        visited[root[0]+1][root[1]] = True
+                        queue.append([root[0]+1, root[1]])
                 
                 if  root[1]+1 < max_width:
-                    queue.append([root[0], root[1]+1])
+                    if not visited[root[0]][root[1]+1]:
+                        visited[root[0]][root[1]+1] = True
+                        queue.append([root[0], root[1]+1])
             
         return False
+#----------------------------------
         
         
-        
-        
+        # dfs 접근: 실패
 #         max_width = len(matrix[0])
 #         max_height = len(matrix)
         
