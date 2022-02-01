cb1973df018db6bf6dd8985c49749b963a442e70
woonys <wodns1324@naver.com>
2022-02-01 23:53:58 +0900

    121

---
+++ python_algorithm_interview/Ch.7/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,36 @@
+'''
+How to solve: 답안 참고
+시간복잡도: O(n) -> for문 1번으로 해결 / min, max 둘다 O(1)
+공간복잡도: O(1) -> in-place
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        for price in prices:
+            max_profit, min_price = 0, float('inf')
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
+        
+        
+        
+        
+        
+        
+        
+        # length = len(prices)
+        # max_val = 0
+        # for i in prices:
+        #     idx = prices.index(i)
+        #     for j in prices[idx+1:]:
+        #         if j-i <0:
+        #             continue
+        #         max_val = max(j-i, max_val)
+        # return max_val
+                
+                    
+                
+            
+            
+            
