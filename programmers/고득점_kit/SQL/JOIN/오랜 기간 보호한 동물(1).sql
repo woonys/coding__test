a4728b14124c93970521a5d9688ffee4b27a1d3b
woonys <wodns1324@naver.com>
2022-03-29 16:24:41 +0900

    오랜 기간 보호한 동물(1)

---
+++ programmers/고득점_kit/SQL/JOIN/오랜 기간 보호한 동물(1).sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ins.name, ins.datetime
+from animal_ins ins
+left outer join animal_outs outs
+on ins.animal_id = outs.animal_id
+where outs.animal_id is null
+order by ins.datetime
+limit 3
