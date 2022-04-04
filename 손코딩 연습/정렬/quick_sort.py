45e26bb8323a5d28e8a579d495db0dcd044c33cb
woonys <wodns1324@naver.com>
2022-04-04 13:21:50 +0900

    quick_sort 연습 1번 더

--- 손코딩 연습/정렬/quick_sort.py
+++ 손코딩 연습/정렬/quick_sort.py
@@ -9,6 +9,9 @@ Quick sort
 '''
 
 
+from turtle import left, right
+
+
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quick_sort(array):
@@ -23,3 +26,18 @@ def quick_sort(array):
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
 print(quick_sort(array))
+
+
+#연습: 1번 더!
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+    
+    leftside = [x for x in tail if x <= pivot]
+    rightside = [x for x in tail if x > pivot]
+    
+    return quick_sort(leftside) + [pivot] + quick_sort(rightside)
+    
