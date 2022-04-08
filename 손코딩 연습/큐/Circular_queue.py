7138fad600efec7c81e24e40dec8725134d4cd40
woonys <wodns1324@naver.com>
2022-04-08 17:05:13 +0900

    circular_queue

---
+++ 손코딩 연습/큐/Circular_queue.py
@@ -0,0 +1,54 @@
+class CircularQueue:
+    def __init__(self, k: int): # k는 queue의 크기
+        self.k = k
+        self.queue = [None] * k
+        self.head = self.tail  = -1
+    
+    # Insert an element into the circular queue
+    def enqueue(self, data):
+        # case 1: 꽉 찼을 때
+        if ((self.tail + 1) % self.k == self.head):
+            print("The circular queue is full\n")
+        
+        # case 2: 아예 텅 비었을 때 
+        elif (self.head == -1): # queue가 비었을 때
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+        # case 3: 빈 공간이 있고 값이 채워져있을 때
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+    
+    # Delete an element from the circular queue
+    def dequeue(self):
+        # case 1: queue가 비었을 때
+        if (self.head == -1):
+            print("The circular queue is empty\n")
+        
+        # case 2: 값이 하나만 있을 때는 head와 tail의 인덱스가 동일
+        elif (self.head == self.tail):
+            temp = self.queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+        
+        else:
+            temp = self.queue[self.head]
+            self.head = (self.head + 1) % self.k
+            return temp
+    
+    def printCQueue(self):
+        if (self.head == -1):
+            print("No element in the circular queue")
+            
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
