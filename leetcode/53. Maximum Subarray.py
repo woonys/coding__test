c7bea87cf5f1a030c7f1f626e8696193a6f98948
woonys <wodns1324@naver.com>
2022-04-18 23:45:23 +0900

    53

---
+++ leetcode/53. Maximum Subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [0] * N
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = nums[i] + (dp[i-1] if dp[i-1]>=0 else 0)
+        return max(dp)
